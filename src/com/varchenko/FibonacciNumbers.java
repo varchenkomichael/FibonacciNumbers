9a2820938d7b430a6d9b02a5c9c07893c6981a08
Varchenko Michael <mishanjav2001@gmail.com>
2021-02-03 12:03:08 +0200

    removed pushed files

---
+++ src/com/varchenko/FibonacciNumbers.java
@@ -0,0 +1,12 @@
+package com.varchenko;
+
+public class FibonacciNumbers {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; ){
+            int n = 1;
+            if (i == 0) return (0);
+            else if (i == 1 ) return ;
+    }
+}
+}
