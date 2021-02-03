10aa7af386c537e9fb647cf0593c32b19dda1198
Varchenko Michael <mishanjav2001@gmail.com>
2021-02-03 13:37:04 +0200

    fibonacci numbers

--- src/com/varchenko/FibonacciNumbers.java
+++ src/com/varchenko/FibonacciNumbers.java
@@ -1,12 +1,15 @@
 package com.varchenko;
 
 public class FibonacciNumbers {
-
-    public static void main(String[] args) {
-        for (int i = 0; i < 50; ){
-            int n = 1;
-            if (i == 0) return (0);
-            else if (i == 1 ) return ;
+    public static void main(String[] args){
+      for (int index = 0; index<25; index++) {
+            System.out.println(f(index));
+        }
+    }
+    public static int f(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        else return f(n-1) + f(n-2);
     }
 }
-}
+
